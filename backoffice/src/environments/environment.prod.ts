88ee61ba0fd38660f954088b73649a2198c1faac
Marcus Vinicius Meirelles <mavifm@gmail.com>
2026-02-05 09:20:53 -0300

    Ajustes Parametros PRD

---
+++ backoffice/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://apibra021store.rlm.dev.br/api'
+};
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://apibra021store.rlm.dev.br/api',
+  mercadoPagoPublicKey: 'TEST-6b4bd744-6665-43b1-a6f1-cbf5d66277e5'
+};
